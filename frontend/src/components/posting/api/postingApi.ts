79329a05676d662100a621d4eaa2440695b5f599
wisdom5565 <wisdom5565@naver.com>
2025-08-08 17:21:26 +0900

    fix : 모집글 등록 반환값 수정

--- frontend/src/components/posting/api/postingApi.ts
+++ frontend/src/components/posting/api/postingApi.ts
@@ -16,7 +16,7 @@ export const postingApi = {
     // 모집글 등록
     addPosting: async (postingData: PostingRequestDTO) => {
         try {
-            return await post<PostingResponseDTO>(`${API_BASE_URL}/posting/`, postingData);
+            return await post<number>(`${API_BASE_URL}/posting/`, postingData);
         } catch (error) {
             console.error('모집글 등록 에러:', error);
             return {
